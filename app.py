bcec82f76f2a77f092c8acc75f848c4af7a0dbeb
celeste-42bit <studi.jasha.gruener@gmail.com>
2023-11-23 00:18:31 +0100

    Added perlin noise generator

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
-
+from numpy import floor
+from perlin_noise import PerlinNoise
 
 app = Ursina()
-window.color = color.rgba(200, 200, 255, 1)
+
 Sky(texture=load_texture("sky_sunset.jpg"))
+
+generator_settings = [42069, 1, 20, 10]  # format [seed, octaves, frequency, amplitude]
+terrain_size = 50
 
 def input(key):
     match key:
@@ -23,19 +27,21 @@ def random_color():
     red = random.Random().random() * 255
     green = random.Random().random() * 255
     blue = random.Random().random() * 255
-    return color.rgba(red, green, blue)
+    return color.rgb(red, green, blue)
 
 terrain = Entity(model = None, collider = None)
 
-terrain_size = 50
-for i in range(terrain_size * terrain_size):
-    block = Entity(model = 'cube', color = random_color(), texture = 'grass')
-    block.x = i/terrain_size
-    block.z = i%terrain_size
-    block.y = 0
+heightmap = PerlinNoise(octaves = generator_settings[1], seed = generator_settings[0])
+
+for n in range(terrain_size * terrain_size):
+    block = Entity(model = 'cube', color = random_color())
+    block.x = floor(n/terrain_size)
+    block.z = floor(n%terrain_size)
+    block.y = floor((heightmap([block.x / generator_settings[2], block.z / generator_settings[2]])) * generator_settings[3])
     block.parent = terrain
 
-terrain.combine(auto_destroy = True)  # do not destroy the children after combine
+terrain.combine(auto_destroy = True)  # destroying children (for now) because of visual glitch. Deactivate after fix.
+terrain.texture = 'grass'
 terrain.collider = 'mesh'
 
 player = FirstPersonController()
